d076bc2beacd4e84539c412da238f0f298e63c0b
Ann Cudis <jvcudis@gmail.com>
2016-04-18 00:11:06 +0800

    Layout and styles

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+(function($) {
+
+  $('a.tab-nav-item').on('click', function(evt) {
+    evt.preventDefault();
+
+    var currentAttrValue = $(this).attr('href')
+        currentEl = $(this).attr('id');
+
+    // Toggle tab content
+    $('.tab-content ' + currentAttrValue).show().siblings().hide();
+    $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+    // Toggle adding of border
+    // Refactor later
+    if (currentEl == 'userTab') {
+      $('#settingTab').toggleClass('right-border');
+      $('#contactTab').toggleClass('no-left-space');
+    } else {
+      $('#settingTab').removeClass('right-border');
+      $('#contactTab').removeClass('no-left-space');
+    }
+
+    if (currentEl == 'contactTab') {
+      $('#userTab').toggleClass('right-border');
+      $('#settingTab').toggleClass('no-left-space');
+    } else {
+      $('#userTab').removeClass('right-border');
+      $('#settingTab').removeClass('no-left-space');
+    }
+
+  })
+
+})(jQuery);
